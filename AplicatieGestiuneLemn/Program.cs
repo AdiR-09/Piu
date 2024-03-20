c6f2f3b5718806ce39031e6d9a49e4cfb625b4ff
adi <adi.roman2003@gmail.com>
2024-03-20 15:37:37 +0200

    in curs de ...

--- AplicatieGestiuneLemn/Program.cs
+++ AplicatieGestiuneLemn/Program.cs
@@ -6,6 +6,7 @@ using EntitatiAngajati; // Pentru proiectul Angajat
 using GestionareMateriePrima; // Pentru proiectul MateriePrima
 using GestiuneStocMateriePrima; // Pentru proiectul StocMateriePrima
 using System.Globalization;
+using NivelStocareDate;
 
 
 namespace GestiuneLemn
@@ -50,7 +51,7 @@ namespace GestiuneLemn
                 switch (optiune)
                 {
                     case "1":
-                        angajatNou = CitireAngajatTastatura();
+                        angajatNou = NivelStocareDateAngajat.CitireAngajatTastatura();
                         ok = 1;
                         break;
 
@@ -61,7 +62,7 @@ namespace GestiuneLemn
 
                     case "3":
                         // Adaugă cod pentru afișarea listei de angajați
-                        AfisareListaAngajati();
+                        NivelStocareDateAngajat.AfisareListaAngajati();
                         break;
 
                     case "4":
@@ -142,63 +143,6 @@ namespace GestiuneLemn
                 }
             } while (optiune != "6");
         }
-
-        public static Angajat CitireAngajatTastatura()
-        {
-            Console.WriteLine("Introduceti numele angajatului:");
-            string nume = Console.ReadLine();
-
-            Console.WriteLine("Introduceti prenumele angajatului:");
-            string prenume = Console.ReadLine();
-
-            Console.WriteLine("Introduceti CNP-ul angajatului:");
-            string cnp = Console.ReadLine();
-
-            Console.WriteLine("Introduceti data angajarii (format: ZZ.LL.AAAA):");
-            DateTime dataAngajarii;
-            if (!DateTime.TryParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dataAngajarii))
-            {
-                Console.WriteLine("Data introdusă nu este în formatul corect. Vă rugăm să reintroduceți data angajării.");
-                return null; // În cazul în care data nu este introdusă corect, returnăm null
-            }
-
-            Console.WriteLine("Introduceti salariul angajatului:");
-            decimal salariu;
-            if (!decimal.TryParse(Console.ReadLine(), out salariu))
-            {
-                Console.WriteLine("Salariul introdus nu este într-un format corect. Vă rugăm să reintroduceți salariul angajatului.");
-                return null; // În cazul în care salariul nu este introdus corect, returnăm null
-            }
-
-            Console.WriteLine("Introduceti departamentul angajatului:");
-            string departament = Console.ReadLine();
-
-            // Creăm un obiect de tip Angajat cu datele introduse și îl returnăm
-            return new Angajat(nume, prenume, cnp, dataAngajarii, salariu, departament);
-        }
-        public static void AfisareListaAngajati()
-        {
-            Console.WriteLine("Lista de angajați:");
-            Console.WriteLine("............................................................................................................");
-            Console.WriteLine("Nr. | Nume                  | Prenume      | CNP           | Data angajării  | Salariu | Departament");
-            Console.WriteLine("............................................................................................................");
-
-            // Verificăm dacă există angajați înregistrați
-            if (nrAngajati == 0)
-            {
-                Console.WriteLine("Nu există angajați înregistrați.");
-                return;
-            }
-
-            // Parcurgem array-ul de angajați și afișăm fiecare angajat
-            for (int i = 0; i < nrAngajati; i++)
-            {
-                Console.WriteLine($"{i + 1,-4}| {angajati[i].Nume,-21}| {angajati[i].Prenume,-14} | {angajati[i].CNP,-14} | {angajati[i].DataAngajarii.ToString("dd/MM/yyyy"),-17}| {angajati[i].Salariu,-8} | {angajati[i].Departament,-11}");
-            }
-            Console.WriteLine("............................................................................................................");
-        }
-
-
         public static void AfisareListaMateriiPrime()
         {
             Console.WriteLine("Lista de materii prime:");
---
+++ NivelStocareDate/NivelStocareDateAngajat.cs
@@ -0,0 +1,72 @@
+﻿using EntitatiAngajati;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NivelStocareDate
+{
+    public class NivelStocareDateAngajat
+    {
+        static int nrAngajati = 0;
+        static Angajat[] angajati = new Angajat[MAX_ANGAJATI];
+         private const int MAX_ANGAJATI = 100;
+        public static Angajat CitireAngajatTastatura()
+        {
+            Console.WriteLine("Introduceti numele angajatului:");
+            string nume = Console.ReadLine();
+
+            Console.WriteLine("Introduceti prenumele angajatului:");
+            string prenume = Console.ReadLine();
+
+            Console.WriteLine("Introduceti CNP-ul angajatului:");
+            string cnp = Console.ReadLine();
+
+            Console.WriteLine("Introduceti data angajarii (format: ZZ.LL.AAAA):");
+            DateTime dataAngajarii;
+            if (!DateTime.TryParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dataAngajarii))
+            {
+                Console.WriteLine("Data introdusă nu este în formatul corect. Vă rugăm să reintroduceți data angajării.");
+                return null; // În cazul în care data nu este introdusă corect, returnăm null
+            }
+
+            Console.WriteLine("Introduceti salariul angajatului:");
+            decimal salariu;
+            if (!decimal.TryParse(Console.ReadLine(), out salariu))
+            {
+                Console.WriteLine("Salariul introdus nu este într-un format corect. Vă rugăm să reintroduceți salariul angajatului.");
+                return null; // În cazul în care salariul nu este introdus corect, returnăm null
+            }
+
+            Console.WriteLine("Introduceti departamentul angajatului:");
+            string departament = Console.ReadLine();
+
+            // Creăm un obiect de tip Angajat cu datele introduse și îl returnăm
+            return new Angajat(nume, prenume, cnp, dataAngajarii, salariu, departament);
+        }
+        public static void AfisareListaAngajati()
+        {
+            Console.WriteLine("Lista de angajați:");
+            Console.WriteLine("............................................................................................................");
+            Console.WriteLine("Nr. | Nume                  | Prenume      | CNP           | Data angajării  | Salariu | Departament");
+            Console.WriteLine("............................................................................................................");
+
+            // Verificăm dacă există angajați înregistrați
+            if (nrAngajati == 0)
+            {
+                Console.WriteLine("Nu există angajați înregistrați.");
+                return;
+            }
+
+            // Parcurgem array-ul de angajați și afișăm fiecare angajat
+            for (int i = 0; i < nrAngajati; i++)
+            {
+                Console.WriteLine($"{i + 1,-4}| {angajati[i].Nume,-21}| {angajati[i].Prenume,-14} | {angajati[i].CNP,-14} | {angajati[i].DataAngajarii.ToString("dd/MM/yyyy"),-17}| {angajati[i].Salariu,-8} | {angajati[i].Departament,-11}");
+            }
+            Console.WriteLine("............................................................................................................");
+        }
+
+    }
+}
